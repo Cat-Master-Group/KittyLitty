cf0bfbda79a457cdc2e691f73c400c5918261f08
toxtlej1 <Jesustoxtle@gmail.com>
2022-08-21 02:40:41 -0400

    feat(conversations): ✨  completed convo front end

--- public/js/components/messages.js
+++ public/js/components/messages.js
@@ -1,52 +1,116 @@
 (function ($) {
-  let messageList = $("#messageList");
-  let enterMessage = $("#enterMessage");
-  let submitMessage = $("#submitMessage");
+  const conversationList = $("#conversationList");
+  const enterMessage = $("#enterMessage");
+  const submitMessage = $("#submitMessage");
+  const messageList = $("#messageList");
 
   let peopleDir;
-  let convoList;
+  let convoList = [];
   let id;
-  let convoObj = [];
+  let curConvo;
+  let curMessageList;
+
+  const syncInterval = 1000 * 2;
+  let grabFail = false;
 
   const requestConfig = {
     method: "GET",
     url: "/api/conversation/messages/all",
   };
 
-  // current active user
-  try {
-    $.ajax(requestConfig).then(function (responseConversations) {
-      convoList = responseConversations.conversations;
-      peopleDir = responseConversations.peopleDir;
-      id = responseConversations.id;
-      console.log(responseConversations);
+  const loadConvo = () => {
+    try {
+      $.ajax(requestConfig).then(function (response) {
+        convoList = response.conversations;
+        peopleDir = response.peopleDir;
+        id = response.id;
+        conversationList.empty();
 
-      convoList.map((c) => {
-        console.log(c.participants);
-        let otherUser;
-        if (c.participants.length === 2) {
-          if (c.participants.indexOf(id) === 0) {
-            otherUser = c.participants[1];
+        convoList.map((c) => {
+          let otherUser;
+          if (c.participants.length === 2) {
+            if (c.participants.indexOf(id) === 0) {
+              otherUser = c.participants[1];
+            } else {
+              otherUser = c.participants[0];
+            }
           } else {
-            otherUser = c.participants[0];
+            // otherUser =
           }
-        } else {
-          // otherUser =
-        }
-        console.log(otherUser);
-        messageList.append(`<li>${c._id}</li>`);
+          conversationList.append(
+            `<li><button class="convo" value=${c._id}>${peopleDir[otherUser]}</button></li>`
+          );
+        });
       });
-      //messageList.append($("<li>").text(enterMessage.val()));
-    });
-  } catch (error) {
-    console.log(error);
-  }
+    } catch (error) {
+      if (!grabFail) {
+        alert("COULD NOT GRAB MESSAGES.");
+      }
+      grabFail = true;
+      console.log(error);
+    }
+  };
 
-  console.log(submitMessage);
+  loadConvo();
+  setInterval(loadConvo, syncInterval);
+
+  const patchRequestConfig = {
+    method: "PATCH",
+    url: "/api/conversation/messages/",
+  };
 
   submitMessage.on("click", function (event) {
     event.preventDefault();
+    if (!curConvo) {
+      alert("SELECT CONVERSATION");
+    } else if (enterMessage.val().trim() === "") {
+      alert("MESSAGE CANNOT BE EMPTY");
+    } else {
+      patchRequestConfig.url = "/api/conversation/messages/" + curConvo;
+      patchRequestConfig.data = { messages: enterMessage.val() };
+      try {
+        $.ajax(patchRequestConfig).then(function (response) {
+          curMessageList = response.messages;
+          populateMessages();
+        });
+      } catch (error) {
+        alert("SEND MESSAGE ERROR: COULD NOT SEND MESSAGE");
+        console.log(error);
+      }
+    }
   });
+
+  conversationList.on("click", "button", function (event) {
+    event.preventDefault();
+
+    curConvo = event.target.value;
+    const findConvo = (el) => {
+      if (el._id === curConvo) {
+        return true;
+      }
+    };
+    let index = convoList.findIndex(findConvo);
+    curMessageList = convoList[index].messages;
+
+    populateMessages();
+  });
+
+  const populateMessages = () => {
+    messageList.empty();
+    for (let i = 0; i < curMessageList.length; i++) {
+      const curMessage = curMessageList[i];
+      messageList.append(
+        `<li class=${curMessage.senderId === id ? "right" : "left"}>${
+          curMessage.messageText
+        }</li>`
+      );
+      messageList.append(
+        `<li class=${curMessage.senderId === id ? "right" : "left"}>${
+          peopleDir[curMessage.senderId]
+        }</li>`
+      );
+    }
+  };
 })(window.jQuery);
 
 // Helper functions
