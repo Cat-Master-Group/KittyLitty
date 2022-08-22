0506dfa6405fe3b07f0db6c04a2389224969b817
Jaeson Valles <jaesonvalles@gmail.com>
2022-08-21 22:51:05 -0400

    feat(header):added header and header options

--- controllers/component.controller.js
+++ controllers/component.controller.js
@@ -1,10 +1,11 @@
-const user = require("../data/user.data");
+const userdb = require("../data/user.data")
+
 
 module.exports = {
   loadSignin(req, res, next) {
     const renderData = {};
     renderData.layout = "component";
-    renderData.axios = req.query.axios;
+    renderData.ajax = req.query.ajax;
     renderData.componentname = "signin";
     renderData.script = true;
     res.render("components/signin", renderData);
@@ -13,42 +14,81 @@ module.exports = {
   loadSignup(req, res, next) {
     const renderData = {};
     renderData.layout = "component";
-    renderData.axios = req.query.axios;
+    renderData.ajax = req.query.ajax
     renderData.componentname = "signup";
     renderData.script = true;
     res.render("components/signup", renderData);
   },
 
+  loadHeaderMenu(req, res, next) {
+    const renderData = {};
+    renderData.layout = "component";
+    renderData.ajax = req.query.ajax
+    renderData.componentname = "header-menu";
+    renderData.script = true;
+    res.render("components/header-menu", renderData);
+  },
+
+  loadFollowedList(req, res, next) {
+    const renderData = {};
+    renderData.layout = "component";
+    renderData.ajax = req.query.ajax
+    renderData.componentname = "followed-list";
+    renderData.script = true;
+
+    const projection = {};
+    projection.projection = {
+      _id: true,
+      userName: true,
+    }
+
+    userdb.getUserArray(req.session.user.followedUsers, projection).then((followedUserList) => {
+      renderData.followedUsers = [];
+      if (followedUserList) {
+        followedUserList.forEach(element => {
+          renderData.followedUsers.push({ _id: element._id.toString(), userName: element.userName });
+        });
+      }
+      res.render("components/followed-list", renderData);
+    });
+  },
+
   loadSettings(req, res, next) {
     const renderData = {};
     renderData.layout = "component";
-    renderData.axios = req.query.axios;
+    renderData.ajax = req.query.ajax
     renderData.componentname = "settings";
     renderData.script = true;
     res.render("components/settings", renderData);
   },
 
-  async loadCatInfo(req, res, next) {
+  loadCatInfo(req, res, next) {
     let id = req.params.id;
     let userInfo = {};
-    userInfo = await user.getUser(id);
-    // TODO
-    const catInfo = userInfo.userCat;
-    const renderData = {};
-    renderData.layout = "component";
-    renderData.axios = req.query.axios;
-    renderData.componentname = "catInfo";
-    renderData.script = true;
-    renderData.catInfo = catInfo;
-    renderData.userInfo = userInfo;
-    res.render("components/catInfo", { renderData });
+    userInfo = userdb.getUser(id);
+
+    const p = Promise.resolve(userInfo);
+
+    p.then(value => {
+
+      userInfo = value;
+      const catInfo = userInfo.userCat;
+      console.log(catInfo);
+      const renderData = {};
+      renderData.layout = "component";
+      renderData.ajax = req.query.ajax;
+      renderData.componentname = "cat-info";
+      renderData.script = false;
+      renderData.catInfo = catInfo;
+      res.render("components/cat-info", renderData);
+    })
   },
 
   //Swipe
-  available(req, res, next) {
+  loadAvailable(req, res, next) {
     const renderData = {};
     renderData.layout = "component";
-    renderData.axios = req.query.axios;
+    renderData.ajax = req.query.ajax
     renderData.componentname = "available";
     renderData.script = true;
     res.render("components/available", renderData);
